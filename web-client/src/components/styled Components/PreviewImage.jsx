c828d467d8bd839d5b82d6816cfa0fec10602625
Arnab Ghosh <arnab@gmail.com>
2023-11-10 23:51:09 +0530

    Fixed Preview Image Component

--- web-client/src/components/styled Components/PreviewImage.jsx
+++ web-client/src/components/styled Components/PreviewImage.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-const PreviewImage = ({base64Image,setBase64Image,image}) => {
-    const reader=new FileReader();
-    reader.readAsDataURL(image);
-    reader.onload=()=>{
-        setBase64Image(reader.result);
-    };
+const PreviewImage = ({ base64Image, setBase64Image, image }) => {
+    if (!image) return;
+    useEffect(() => {
+        const reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = () => {
+            setBase64Image(reader.result);
+        };
+        // Clean up the FileReader on component unmount or if image changes
+        return () => {
+            reader.abort();
+        };
+    }, [image, setBase64Image]);
+
     return (
-    <>
         <img src={base64Image} alt="avatar" />
-    </>
-  )
-}
+    );
+};
 
-export default PreviewImage
+export default PreviewImage;
