b2ef127d5d66e96f5f171669b7602e504103ba7e
Arnab Ghosh <arnab@gmail.com>
2023-11-11 14:16:10 +0530

    Added Responsivess in the Globe Component

--- web-client/src/components/globe/Globe.jsx
+++ web-client/src/components/globe/Globe.jsx
@@ -1,13 +1,15 @@
 import React ,{Suspense} from 'react'
+import styles from './Globe.module.scss'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
+import CanvasLoader from './Loader';
 const Globe = () => {
   const earth=useGLTF('./planet/scene.gltf')
   return (
     <primitive
       object={earth.scene}
-      scale={3}
-      position-y={0}
+      scale={3.3}
+      position-x={0}
       rotation-y={0}
     />
   )
@@ -15,15 +17,16 @@ const Globe = () => {
 
 const EarthCanvas=()=>{
   return(
-    <div style={{width:"500px",height:"500px"}}>
+    <div className={styles.globeContainer}>
+      <div className={styles.globeCanvas}>
       <Canvas shadows frameloop='demand' gl={{preserveDrawingBuffer:true}} camera={{fov:45,naear:0.1,far:200,position:[-4,3,6]}}>
-        <Suspense>
+        <Suspense fallback={<CanvasLoader/>}>
           <OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2} />
           <Globe/>
         </Suspense>
       </Canvas>
+      </div>
     </div>
-    
   )
 }
 
