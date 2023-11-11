2729375ec8e2f1a0074b562fe783be90b30b2715
Arnab Ghosh <arnab@gmail.com>
2023-11-11 14:20:07 +0530

    Added Responsiveness in the auth component

--- web-client/src/pages/auth/Auth.jsx
+++ web-client/src/pages/auth/Auth.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './Auth.module.scss'
+import React from 'react';
+import styles from './Auth.module.scss';
 import Register from './Register';
 import Login from './Login';
 import { useParams } from 'react-router-dom';
@@ -7,14 +7,15 @@ import EarthCanvas from '../../components/globe/Globe';
 
 const Auth = () => {
     const { page } = useParams();
-  return (
-    <div className={styles.authBackground}>
-        <div className={styles.authContainer}>
-            <EarthCanvas/>
-            {page === 'register' ? <Register/> : <Login/>}
+    return (
+        <div className={styles.authBackground}>
+            <div className={styles.authContainer}>
+                <div className={styles.conatinerBox}>
+                    <EarthCanvas />
+                    {page === 'register' ? <Register /> : <Login />}
+                </div>
+            </div>
         </div>
-    </div>
-  )
-}
-
-export default Auth
+    );
+};
+export default Auth;
