3f6dbd1f7ef09dc80f4a7ef5585fef0df221a29b
Arnab Ghosh <arnab@gmail.com>
2023-11-12 11:58:03 +0530

    Fixed Contacts component

--- web-client/src/components/contacts/Contacts.jsx
+++ web-client/src/components/contacts/Contacts.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import styles from './Contacts.module.scss'
-import FollowUser from '../action buttons/FollowUser'
+import NoProfilePic from '../../assets/account.png'
+
 import { useSelector } from 'react-redux'
-import useAxiosPrivate from '../../hooks/useAxiosPrivate'
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { CircularProgress } from '@mui/material'
+
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
 import WigetWrapper from '../styled Components/wiget wrapper/WegetWrapper'
-import NoProfilePic from '../../assets/account.png'
+import FollowUser from '../action buttons/FollowUser'
+
 const Contacts = () => {
     const axiosPrivate=useAxiosPrivate();
-    const token=useSelector((state)=>state.token);
     const currentUser=useSelector((state)=>state.currentUser);
 
     const [following, setFollowing] = useState([]);
@@ -24,6 +26,9 @@ const Contacts = () => {
             const res = await axiosPrivate.get(`/users/following/${currentUser._id}?page=${pageNo}&limit=${limit}`)
             const mergedUsers=[...following,...res.data];
             setFollowing(mergedUsers);
+            if(res.data.length===0){
+                setHasMoreUsers(false);
+            }
           } catch (error) {
             console.log(error);
         }
@@ -39,35 +44,35 @@ const Contacts = () => {
   return (
     <WigetWrapper>
         <span className='primaryText' style={{fontSize:"16px"}}>Contacts</span>
-        <InfiniteScroll
-            dataLength={following.length}
-            next={fetchMoreData}
-            hasMore={hasMoreUsers}
-            //loader={<CircularProgress/>}
-            height={1}
-            //endMessage={<h4>You have reached the end</h4>}
-            style={{ display: 'flex', flexDirection:"column",maxHeight:"300px", alignItems: 'center', gap:'20px',height:"100%"}}
-            //scrollableTarget="scrollableFollowingDiv"
-        >
-            {following.length > 0 ? (
-                following.map((user) => (
-                    <div className={styles.contact} key={user.userId}>
-                        <div className={styles.contactInfo}>
-                            <img src={user.profilePicture ? user.profilePicture : NoProfilePic} alt="User Profile" />
-                            <div className={styles.userInfo}>
-                                <span className='primaryText'>@{user.userName}</span>
-                                <span className='secondaryText'>{user.fullName}</span>
+        <div id='scrollableFollowingDiv' className={styles.contactsContainer}>
+            <InfiniteScroll
+                dataLength={following.length}
+                next={fetchMoreData}
+                hasMore={hasMoreUsers}
+                loader={<CircularProgress/>}
+                style={{ display: 'flex', flexDirection:"column", alignItems: 'center',width:"100%", gap:'20px'}}
+                scrollableTarget="scrollableFollowingDiv"
+            >
+                {hasMoreUsers || following.length>0 ?(
+                    following.map((user) => (
+                        <div className={styles.contact} key={user.userId}>
+                            <div className={styles.contactInfo}>
+                                <img src={user.profilePicture ? user.profilePicture : NoProfilePic} alt="User Profile" />
+                                <div className={styles.userInfo}>
+                                    <span className='primaryText'>@{user.userName}</span>
+                                    <span className='secondaryText'>{user.fullName}</span>
+                                </div>
+                            </div>
+                            <div className={styles.contactActions}>
+                                <FollowUser userId={user.userId} currentUser={currentUser} />
                             </div>
                         </div>
-                        <div className={styles.contactActions}>
-                            <FollowUser userId={user.userId} currentUser={currentUser} token={token} />
-                        </div>
-                    </div>
-                ))
-            ) : (
-                <span style={{fontSize:"14px"}} className='secondaryText'>No User Found</span>
-            )}
-        </InfiniteScroll>
+                    ))
+                ) : (
+                    <span style={{fontSize:"14px"}} className='secondaryText'>No User Found</span>
+                )}
+            </InfiniteScroll>
+        </div>
     </WigetWrapper>
   )
 }
