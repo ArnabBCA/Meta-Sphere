388ffb0c4c1cc2e442e970ff425ea5adfa1b5e7e
Arnab Ghosh <arnab@gmail.com>
2023-11-24 13:26:27 +0530

    Disabel React dev tools

--- web-client/src/index.jsx
+++ web-client/src/index.jsx
@@ -5,9 +5,13 @@ import authReducer from './state/index.js';
 
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const store = configureStore({
   reducer: authReducer,
+  devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
