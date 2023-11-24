d8ed67bddef935eaf72f2b3191c06f7bdf7ac66d
Arnab Ghosh <arnab@gmail.com>
2023-11-24 12:54:49 +0530

    Fixed Typo

--- api/middleware/cloudinary.js
+++ api/middleware/cloudinary.js
@@ -5,7 +5,7 @@ dotenv.config();
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
   api_key: process.env.API_KEY,
-  api_secret: '_jUoItwmXAWzSDTUNkGyLb7IIgg' 
+  api_secret: process.env.API_SECRET
 });
 
 module.exports = cloudinary;
